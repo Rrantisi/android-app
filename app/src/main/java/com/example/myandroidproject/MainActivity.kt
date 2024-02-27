6c5c6d5a3e6f3a4d4107e1f9a6912d56a4e42c4a
Rrantisi <rantisireham19@gmail.com>
2024-02-27 18:49:15 -0500

    first commit

---
+++ app/src/main/java/com/example/myandroidproject/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.myandroidproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myandroidproject.ui.theme.MyAndroidProjectTheme
+import net.skyscanner.backpack.compose.card.BpkCard
+import net.skyscanner.backpack.compose.card.BpkCardCorner
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyAndroidProjectTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyCard(){
+    BpkCard(corner = BpkCardCorner.Large) {
+        Text(text = "This is a BpkCard!!")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name! hey there!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyAndroidProjectTheme {
+        Greeting("Android")
+    }
+}
